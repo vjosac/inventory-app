0b8db5356babf93401577dcc4983c85eb41e79c9
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2025-05-21 14:47:59 +0200

    Implement screens for inventory and setup room database, entity, and dao

---
+++ app/src/main/java/com/example/inventory/item/ItemEntryViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.inventory.item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.Item
+import java.text.NumberFormat
+
+class ItemEntryViewModel : ViewModel() {
+    var itemUiState by mutableStateOf(ItemUiState())
+        private set
+
+    fun updateUiState(itemDetails: ItemDetails) {
+        itemUiState =
+            ItemUiState(
+                itemDetails = itemDetails,
+                isEntryValid = validateInput(itemDetails)
+            )
+    }
+
+    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
+        }
+    }
+}
+
+data class ItemUiState(
+    val itemDetails: ItemDetails = ItemDetails(),
+    val isEntryValid: Boolean = false
+)
+
+data class ItemDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val price: String = "",
+    val quantity: String = ""
+)
+
+fun ItemDetails.toItem(): Item = Item(
+    id = id,
+    name = name,
+    price = price.toDoubleOrNull() ?: 0.0,
+    quantity = quantity.toIntOrNull() ?: 0
+)
+
+fun Item.formatedPrice(): String {
+    return NumberFormat.getCurrencyInstance().format(price)
+}
+
+fun Item.toItemDetails(): ItemDetails = ItemDetails(
+    id = id,
+    name = name,
+    price = price.toString(),
+    quantity = quantity.toString()
+)
+
+fun Item.toItemUiState(isEntryValid: Boolean = false): ItemUiState = ItemUiState(
+    itemDetails = this.toItemDetails(),
+    isEntryValid = isEntryValid
+)
